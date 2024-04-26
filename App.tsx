032664ccbbfc23a19b5890281d1ad06181529912
YuInseo <inseo0121@gmail.com>
2024-04-27 02:29:44 +0900

    feat: init project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text} from 'react-native';
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {NavigationContainer} from "@react-navigation/native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
+    return <Text>Home</Text>;
 }
 
+function SearchScreen() {
+    return <Text>Search</Text>;
+}
+
+function NotificationScreen() {
+    return <Text>Notification</Text>;
+}
+
+function MessageScreen() {
+    return <Text>Message</Text>;
+}
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator initialRouteName="Home">
+                <Tab.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{
+                        title: 'Workspace',
+                    }}
+                />
+                <Tab.Screen
+                    name="Search"
+                    component={NotificationScreen}
+                    options={{
+                        title: 'Teamspace',
+
+                    }}
+                />
+                <Tab.Screen
+                    name="Notification"
+                    component={SearchScreen}
+                    options={{
+                        title: 'Notification',
+
+                    }}
+                />
+                <Tab.Screen
+                    name="Message"
+                    component={MessageScreen}
+                    options={{
+                        title: 'Profile',
+
+                    }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#ff9898',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
